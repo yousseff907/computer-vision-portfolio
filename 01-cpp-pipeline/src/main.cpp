6a75cf1c236f9d9cedf3599d1c9d51c4c7e90f27
Youssef <youssef.itani.11.11@gmail.com>
2026-04-15 23:36:22 +0300

    day 1: load image, inspect properties, convert to grayscale

---
+++ 01-cpp-pipeline/src/main.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "Enter an image path" << std::endl;
+		return (1);
+	}
+	cv::Mat image = cv::imread(argv[1]);
+	if (image.empty())
+	{
+		std::cout << "Image is Empty" << std::endl;
+		return (1);
+	}
+	std::cout << "Width       " << image.cols << std::endl;
+	std::cout << "Height      " << image.rows << std::endl;
+	std::cout << "Channels    " << image.channels() << std::endl;\
+	std::cout << "Total Bytes " << image.total() * image.elemSize() << std::endl;
+
+
+	cv::Mat gray;
+	cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+	std::cout << "Gray Channels " << gray.channels() << std::endl;
+
+	std::cout << "Creating GrayScale Image..." << std::endl;
+	cv::imwrite("../GrayScale.jpg", gray);
+	std::cout << "Done" << std::endl;
+	return (0);
+}
